ea2b52a5f33b60d42c6e94ba5a274762d580b532
Cristian Gonzalez <36319296+nagash02@users.noreply.github.com>
2021-12-14 23:11:58 -0300

    Update ApiEjemploTest.java

--- src/test/java/cl/trabajando/api_ejemplo/ApiEjemploTest.java
+++ src/test/java/cl/trabajando/api_ejemplo/ApiEjemploTest.java
@@ -6,8 +6,25 @@ import org.junit.Test;
 
 public class ApiEjemploTest {
 
+   
+	@Test
+    public void test1() {
+	//use junit -test
+    }
     @Test
-    public void test() {
+    public void test2() {
+	
+    }
+    @Test
+    public void test3() {
+	
+    }
+    @Test
+    public void test4() {
+	
+    }
+    @Test
+    public void test5() {
 	
     }
 

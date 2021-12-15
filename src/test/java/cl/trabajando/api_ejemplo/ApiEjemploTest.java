03ba98fb4bff9d0950a6e6049aec17cc71d08eac
Cristian Rodrigo Gonzalez Garce <CristianGonzalez.Globant@latam.com>
2021-12-14 22:57:42 -0300

    test

---
+++ src/test/java/cl/trabajando/api_ejemplo/ApiEjemploTest.java
@@ -0,0 +1,14 @@
+package cl.trabajando.api_ejemplo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ApiEjemploTest {
+
+    @Test
+    public void test() {
+	
+    }
+
+}
